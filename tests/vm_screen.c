c84f194c8b836e1be0518f79a1a7753b6b8c45f1
Peter Evans <pevans@activecampaign.com>
2017-12-18 14:46:40 -0600

    Get our test suite working with SDL
    Had to remove a couple of screen tests for now; it needs a big re-think
    now that we're using SDL. Namely, how do we test SDL?

--- tests/vm_screen.c
+++ tests/vm_screen.c
@@ -4,36 +4,23 @@
 
 Test(vm_screen, create) {
     vm_screen *screen;
+    int x = 320;
+    int y = 240;
+    int scale = 2;
 
-    screen = vm_screen_create();
+    screen = vm_screen_create(x, y, scale);
     cr_assert_neq(screen, NULL);
 
-    cr_assert_eq(screen->color_red, 0);
-    cr_assert_eq(screen->color_blue, 0);
-    cr_assert_eq(screen->color_green, 0);
-    cr_assert_eq(screen->color_alpha, 0);
+    cr_assert_eq(screen->xcoords, x);
+    cr_assert_eq(screen->ycoords, y);
+    cr_assert_eq(screen->scale, scale);
+
+    cr_assert_eq(screen->window, NULL);
+    cr_assert_eq(screen->render, NULL);
+    cr_assert_eq(screen->rect.x, 0);
+    cr_assert_eq(screen->rect.y, 0);
+    cr_assert_eq(screen->rect.w, 0);
+    cr_assert_eq(screen->rect.h, 0);
 
     vm_screen_free(screen);
-}
-
-Test(vm_screen, set_color) {
-    vm_screen *screen;
-    int red = 0xDE;
-    int green = 0xAD;
-    int blue = 0xBE;
-    int alpha = 0xEF;
-
-    screen = vm_screen_create();
-    vm_screen_set_color(screen, red, green, blue, alpha);
-
-    cr_assert_eq(screen->color_red, red);
-    cr_assert_eq(screen->color_green, green);
-    cr_assert_eq(screen->color_blue, blue);
-    cr_assert_eq(screen->color_alpha, alpha);
-
-    vm_screen_free(screen);
-}
-
-Test(vm_screen, draw_rect) {
-    // Nothing to do here...
 }
