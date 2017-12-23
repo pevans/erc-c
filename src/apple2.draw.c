7b19279f1d1267fd0c161d5a469d9529c9d30693
Peter Evans <pevans@activecampaign.com>
2017-12-22 20:45:26 -0600

    No need for this comment any longer

--- src/apple2.draw.c
+++ src/apple2.draw.c
@@ -9,10 +9,6 @@ apple2_draw_pixel(apple2 *mach, vm_16bit addr)
 {
 }
 
-// Drawing text is a two-step process; first you need to wipe out
-// the block on the screen at that point. Then you need to draw the
-// text in over that.
-
 void
 apple2_draw_text(apple2 *mach, vm_16bit addr)
 {
