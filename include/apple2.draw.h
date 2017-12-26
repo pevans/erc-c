68084cc0c39f0f0758241c61f66c285e63c64794
Peter Evans <pevans@activecampaign.com>
2017-12-26 16:43:08 -0600

    Add apple2 header file

--- include/apple2.draw.h
+++ include/apple2.draw.h
@@ -1,6 +1,9 @@
 #ifndef _APPLE2_DRAW_H_
 #define _APPLE2_DRAW_H_
 
+#include "apple2.h"
+#include "vm_bits.h"
+
 extern void apple2_draw_pixel(apple2 *, vm_16bit);
 extern void apple2_draw_text(apple2 *, vm_16bit);
 
