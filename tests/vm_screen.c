62035d03b85fea0476873422964620d4b1402824
Peter Evans <pevans@activecampaign.com>
2018-01-07 20:08:27 -0600

    Remove test (which is now done from vm_area.c)

--- tests/vm_screen.c
+++ tests/vm_screen.c
@@ -68,14 +68,3 @@ Test(vm_screen, ycoords)
     cr_assert_eq(screen->ycoords, 234);
 }
 
-Test(vm_screen, area_set)
-{
-    vm_area area;
-
-    vm_area_set(&area, 1, 2, 3, 4);
-    cr_assert_eq(area.xoff, 1);
-    cr_assert_eq(area.yoff, 2);
-    cr_assert_eq(area.width, 3);
-    cr_assert_eq(area.height, 4);
-}
-
