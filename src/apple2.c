e3ab043aeebcc84ed79979347340f6302dfde485
Peter Evans <pevans@activecampaign.com>
2018-01-09 20:58:53 -0600

    We no longer need to pass the opcode here

--- src/apple2.c
+++ src/apple2.c
@@ -320,7 +320,7 @@ apple2_run_loop(apple2 *mach)
 
     while (vm_screen_active(mach->screen)) {
         mos6502_dis_opcode(mach->cpu, stdout, mach->cpu->PC);
-        mos6502_execute(mach->cpu, mos6502_next_byte(mach->cpu));
+        mos6502_execute(mach->cpu);
         vm_screen_refresh(mach->screen);
     }
 }
