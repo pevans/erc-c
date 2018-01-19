e286ba34f5edb2d90c65391999a98f2e1d4ed9e9
Peter Evans <pevans@activecampaign.com>
2018-01-18 22:19:31 -0600

    Greatly increase counter

--- src/vm_screen.c
+++ src/vm_screen.c
@@ -168,7 +168,7 @@ vm_screen_active(vm_screen *screen)
 {
     SDL_Event event;
     char ch;
-    static int counter = 300;
+    static int counter = 2000;
 
     // There may be _many_ events in the queue; for example, you may be
     // facerolling on Zork because it feels good. And good for you if
