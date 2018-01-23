522905b39c1e461fca7ae0a3d421215733a30542
Peter Evans <pevans@activecampaign.com>
2018-01-23 16:24:46 -0600

    Carry must be unset in this test

--- tests/mos6502.addr.c
+++ tests/mos6502.addr.c
@@ -52,6 +52,7 @@ Test(mos6502_addr, addr_mode_aby_carry0)
     SET_PC_BYTE(cpu, 1, 0x30);
     SET_PC_BYTE(cpu, 2, 0x12);
     cpu->Y = 4;
+    cpu->P = cpu->P & ~MOS_CARRY;
     cr_assert_eq(mos6502_resolve_aby(cpu), 111);
 }
 
@@ -61,7 +62,6 @@ Test(mos6502_addr, addr_mode_aby_carry1)
     SET_PC_BYTE(cpu, 1, 0x30);
     SET_PC_BYTE(cpu, 2, 0x12);
     cpu->Y = 3;
-    cpu->P = cpu->P | MOS_CARRY;
     cr_assert_eq(mos6502_resolve_aby(cpu), 111);
 }
 
