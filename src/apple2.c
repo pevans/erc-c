b97af6cb1693dbd425b7ae2d857d1c949e995f3d
Peter Evans <pevans@activecampaign.com>
2018-01-22 21:43:01 -0600

    Disable active refresh

--- src/apple2.c
+++ src/apple2.c
@@ -324,8 +324,8 @@ apple2_run_loop(apple2 *mach)
     while (vm_screen_active(mach->screen)) {
         mos6502_dis_opcode(mach->cpu, stdout, mach->cpu->PC);
         mos6502_execute(mach->cpu);
-        apple2_draw_40col(mach);
-        vm_screen_refresh(mach->screen);
+//        apple2_draw_40col(mach);
+//        vm_screen_refresh(mach->screen);
     }
 }
 
