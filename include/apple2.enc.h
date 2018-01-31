f87f77b806f1c57cdfdfff246b8b27030b8c4297
Peter Evans <pevans@activecampaign.com>
2018-01-31 15:58:57 -0600

    Add ENC_VOLUME definition

--- include/apple2.enc.h
+++ include/apple2.enc.h
@@ -4,6 +4,13 @@
 #include "vm_bits.h"
 #include "vm_segment.h"
 
+/*
+ * This is a magic number for the disk volume; I haven't experimented
+ * with using other values for it, and this one in particular is used in
+ * WinApple, so I went with it.
+ */
+#define ENC_VOLUME  0xfe
+
 extern int apple2_enc_4n4(vm_segment *, int, vm_8bit); 
 extern int apple2_enc_sector(vm_segment *, vm_segment *, int, int);
 extern int apple2_enc_sector_header(vm_segment *, int, int, int);
