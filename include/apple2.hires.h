47bbed1399904a529fd756119156d33bd5a8860d
Peter Evans <peter@modul8r.com>
2018-03-14 00:05:26 -0500

    Add color tables, row table

--- include/apple2.hires.h
+++ include/apple2.hires.h
@@ -2,7 +2,12 @@
 #define _APPLE2_HIRES_H_
 
 #include "apple2.h"
+#include "vm_bits.h"
 
+extern vm_color apple2_hires_color_h0(vm_8bit);
+extern vm_color apple2_hires_color_h1(vm_8bit);
 extern void apple2_hires_draw(apple2 *, size_t);
+extern int apple2_hires_row(size_t);
+extern int apple2_hires_col(size_t);
 
 #endif
