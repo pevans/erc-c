ee2c9746c61abc5a8f72d05746686b4f91f1d290
Peter Evans <peter@modul8r.com>
2018-04-07 19:54:05 -0500

    Add docblocks

--- src/apple2/event.c
+++ src/apple2/event.c
@@ -20,12 +20,19 @@ apple2_event_init()
     vm_di_set(VM_DEBUG_FUNC, apple2_event_debug);
 }
 
+/*
+ * Pause the running state of the apple2 machine
+ */
 EVENT_DO(apple2_event_pause)
 {
     apple2 *mach = (apple2 *)_mach;
     mach->paused = !mach->paused;
 }
 
+/*
+ * Enter into a debugging state. This also implies a pause in execution
+ * (which can be resumed in the debugger).
+ */
 EVENT_DO(apple2_event_debug)
 {
     apple2 *mach = (apple2 *)_mach;
